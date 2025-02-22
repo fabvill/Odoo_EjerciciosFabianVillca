71dc30cc038b3b297ce2051104d67b185b240822
Fabian Villca <fabian.villca@educa.madrid.org>
2025-02-22 20:50:41 +0100

    Commit 5 Creacion de vistas básicas en desarrollo en Odoo

--- models/real_estate.py
+++ models/real_estate.py
@@ -4,4 +4,30 @@ class RealEstate(models.Model):
     _name = "real.estate"
     _description = "Real Estate Module"
 
-    name = fields.Char(default="House", required=True)
+    active = fields.Boolean(default=True, invisble=True) 
+    name = fields. Char(required=True) 
+    state = fields.Selection(
+        [
+            ("new", "New"),
+            ("received", "Offer Received"),
+            ("accepted", "Offer Accepted"), ("sold", "Sold"),
+            ("canceled", "Canceled"),
+            
+        ],
+        required=True,
+        copy=False,
+        default="new"
+    )
+    postcode = fields.Char()
+
+    def _default_date (self):
+        return fields.Date.today()
+
+
+    date_availability = fields.Date(default=_default_date) 
+    expected_price = fields. Float()
+    best_offer = fields.Float() 
+    selling_price = fields.Float()
+    description = fields.Text() 
+    bedrooms = fields.Integer() 
+    living_area = fields.Integer()
